cc7178c04b71cfc0e0467d16a66c68aafc164268
Joseph <gadiazajoseph18@gmail.com>
2023-06-02 14:27:57 +0800

    continue the design in features section

--- src/components/features/Card.js
+++ src/components/features/Card.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({title, des, icon}) => {
   return (
-    <div className="w-full h-80 px-12 py-10 rounded-lg shadow-shadowOne flex items-center bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-black hover:to-[#1e2024] transition-colors duration-100">
-
+    <div className="w-full h-80 px-10 py-8 rounded-lg shadow-shadowOne flex items-center bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-black hover:to-[#1e2024] transition-colors duration-100 group">
+      <div className='h-72 overflow-y-hidden'>
+        <div className='flex flex-col gap-6 translate-y-14 group-hover:translate-y-0 transition-transform duration-500'>
+          <div>
+            <span className='text-5xl text-designColor'>{icon}</span>
+          </div>
+          <div className='flex flex-col gap-6'>
+            <h2 className="text-2xl font-titleFont font-bold text-gray-300">{title}</h2>
+            <p className='base'>{des}</p>
+            <span>
+              <div className='w-full border-b-[2px] border-b-designColor'></div>
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AiFillAppstore } from "react-icons/ai";
-import { FaMobile, FaGlobe } from "react-icons/fa";
+import { FaMobile, FaGlobe, FaBars } from "react-icons/fa";
 import { SiProgress, SiAntdesign } from "react-icons/si";
 import Title from '../layouts/Title';
 import Card from './Card';
@@ -8,37 +8,37 @@ import Card from './Card';
 const Features = () => {
   return (
     <section id='features' className='w-full py-20 border-b-[1px] border-b-black'>
-      <Title title='Features' desc='What I Do' />
+      <Title title='Features' desc='What I Do?' />
       <div className='grid grid-cols-3 gap-20'>
         <Card 
-          title=""
-          des=""
-          icon={<AiFillAppstore />}
-        />
-        <Card 
-          title=""
-          des=""
-          icon={<FaMobile />}
-        />
-        <Card 
-          title=""
-          des=""
+          title="Website/App Design"
+          des="Translate visual concepts into functional interfaces, then create wireframes, mockups, and prototypes, ensuring that the design is aesthetically pleasing and user-friendly."
           icon={<FaGlobe />}
         />
         <Card 
-          title=""
-          des=""
+          title="HTML/CSS Coding"
+          des="Write HTML (Hypertext Markup Language) and CSS (Cascading Style Sheets) code to structure and style the content of web pages. HTML defines the page structure, while CSS determines the visual presentation."
           icon={<SiProgress />}
         />
         <Card 
-          title=""
-          des=""
-          icon={<SiAntdesign />}
+          title="JavaScript Development"
+          des="Use JavaScript to enhance the interactivity and functionality of websites, then implement features like dropdown menus, image sliders, form validation, animations, and other interactive elements."
+          icon={<FaBars />}
         />
         <Card 
-          title=""
-          des=""
+          title="Responsive Design"
+          des="Ensure that websites and applications are optimized for different devices and screen sizes, then use responsive design techniques, and flexible images, to create a consistent and accessible experience."
+          icon={<FaMobile />}
+        />
+        <Card 
+          title="Browser Compatibility"
+          des="Test and ensure that websites work correctly across various web browsers (e.g., Chrome, Firefox, Safari, Edge) and their different versions."
           icon={<AiFillAppstore />}
+        />
+        <Card 
+          title="Bug Fixing and Testing"
+          des="Identify and fix issues, bugs, and inconsistencies in the UI, and conduct thorough testing to ensure proper functionality across different scenarios, devices, and browsers."
+          icon={<SiAntdesign />}
         />
       </div>
     </section>
