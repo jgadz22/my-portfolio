746caef359e017853f87fa8895195fddec240e64
Joseph <gadiazajoseph18@gmail.com>
2023-06-19 17:29:41 +0800

    adjust some design and codes

--- src/components/resume/Experience.js
+++ src/components/resume/Experience.js
@@ -93,252 +93,122 @@ const Experience = () => {
         <p className="text-sm text-designColor tracking-[4px]">2017 - 2023</p>
         <h2 className="text-4xl font-bold">Work Experience</h2>
       </div>
-      <div className="w-full h-full items-center justify-center gap-5">
+      <div className="w-full items-center justify-center gap-5">
         <Slider {...settings}>
-          <div className="w-full h-full md:w-[90%] mdl:w-[85%] flex items-center justify-center">
-            <ExperienceCard 
-              position = "IT Specialist"
-              year = "(Jul 2022 - Jan 2023)"
-              company = "Metro Clark International Development Corp. (MCIDC) - Pampanga, Philippines"
-              description = {
-                <ul className="list-disc">
-                  <li>
-                    <p>
-                      Troubleshoot and solve problems quickly to ensure
-                      business operations run smoothly.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Install all new technology for the company or for new
-                      hires including computer operating systems, software
-                      programs and applications.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      General understanding of basic computer terminology
-                      and a strong passion to learn.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Provides ongoing network support, addresses
-                      availability issues, overcomes performance problems.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Reports on overall network performance including
-                      frequent problems, underlying issues and plans
-                      toimprove network availability, performance and
-                      problem resolution.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Ensuring stable operation of existing network
-                      equipment is crucial for minimizing downtime,
-                      improving user experience, and enhancing security.
-                    </p>
-                  </li>
-                </ul>
-              }
+          <div className="w-full md:w-[90%] mdl:w-[85%] flex items-center justify-center">
+            <ExperienceCard
+              position="IT Specialist"
+              year="(Jul 2022 - Jan 2023)"
+              company="Metro Clark International Development Corp. (MCIDC) - Pampanga, Philippines"
+              description={[
+                "Troubleshoot and solve problems quickly to ensure business operations run smoothly.",
+
+                "Install all new technology for the company or for new hires including computer operating systems, software programs and applications.",
+
+                "General understanding of basic computer terminology and a strong passion to learn.",
+
+                "Provides ongoing network support, addresses availability issues, overcomes performance problems.",
+
+                "Reports on overall network performance including frequent problems, underlying issues and plans toimprove network availability, performance and problem resolution.",
+
+                "Ensuring stable operation of existing network equipment is crucial for minimizing downtime, improving user experience, and enhancing security.",
+              ]}
             />
           </div>
           <div>
-            <ExperienceCard 
-              position = "Sales Associate"
-              year = "(Jul 2021 – Jul 2022)"
-              company = "All About Baking, SM Cabanatuan - Nueva Ecija, Philippines"
-              description = {
-                <ul className="list-disc">
-                  <li>
-                    <p>
-                      Assess customers’ needs and provide assistance and information on product features
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Maintain in-stock and presentable condition assigned areas, to follow and achieve department’s sales goals on a monthly, quarterly and yearly basis.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                    Remain knowledgeable on products offered and discuss available options.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Process POS (point of sale) purchases.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Team up with co-workers to ensure proper customer service and actively seek out customers in store.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Handle returns of merchandise and comply with inventory control procedures.
-                    </p>
-                  </li>
-                </ul>
-              }
+            <ExperienceCard
+              position="Sales Associate"
+              year="(Jul 2021 – Jul 2022)"
+              company="All About Baking, SM Cabanatuan - Nueva Ecija, Philippines"
+              description={[
+                "Assess customers’ needs and provide assistance and information on product features.",
+
+                "Maintain in-stock and presentable condition assigned areas, to follow and achieve department’s sales goals on a monthly, quarterly and yearly basis.",
+
+                "Remain knowledgeable on products offered and discuss available options.",
+
+                "Process POS (point of sale) purchases.",
+
+                "Team up with co-workers to ensure proper customer service and actively seek out customers in store.",
+
+                "Handle returns of merchandise and comply with inventory control procedures.",
+              ]}
             />
           </div>
           <div>
-            <ExperienceCard 
-              position = "Stock Clerk"
-              year = "(Nov 2020 – Dec 2020)"
-              company = "Toy Kingdom Express, SM Cabanatuan - Philippines"
-              description = {
-                <ul className="list-disc">
-                  <li>
-                    <p>
-                    Assisting in checking the correctness and condition of deliveries.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                    Transferring stock inside the warehouse.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                    Labelling items for pricing, storage, and transfer.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Notifying supervisors and managers of inventory levels.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Supporting the warehouse manager in the course of their duties.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Following state and federal safety standards.
-                    </p>
-                  </li>
-                </ul>
-              }
+            <ExperienceCard
+              position="Stock Clerk"
+              year="(Nov 2020 – Dec 2020)"
+              company="Toy Kingdom Express, SM Cabanatuan - Philippines"
+              description={[
+                "Assisting in checking the correctness and condition of deliveries.",
+
+                "Transferring stock inside the warehouse.",
+
+                "Labelling items for pricing, storage, and transfer.",
+
+                "Notifying supervisors and managers of inventory levels.",
+
+                "Supporting the warehouse manager in the course of their duties.",
+
+                "Following state and federal safety standards.",
+              ]}
             />
           </div>
           <div>
-            <ExperienceCard 
-              position = "Data Entry or Encoder"
-              year = "(Mar 2019 – Mar 2020)"
-              company = "St. Bernadette Credit and Lending Corporation - Nueva Ecija, Philippines"
-              description = {
-                <ul className="list-disc">
-                  <li>
-                    <p>
-                      Accurately and efficiently encode all data that needs organizing and recording.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Transcribe, scan or photocopy hard copy documents and assure files are properly prepared and saved to backup drives.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Perform any other office tasks that management requires assistance with (checking/cleaning machine if needed, answering phones, etc.)
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Report any major errors or inconsistencies to upper management and maintain report logs of in-progress and/or completed work.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Adhere to and meet set schedules and deadlines by input, track, and maintain all encoded data.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Self-audit my work checking for errors or duplication.
-                    </p>
-                  </li>
-                </ul>
-              }
+            <ExperienceCard
+              position="Data Entry or Encoder"
+              year="(Mar 2019 – Mar 2020)"
+              company="St. Bernadette Credit and Lending Corporation - Nueva Ecija, Philippines"
+              description={[
+                "Accurately and efficiently encode all data that needs organizing and recording.",
+
+                "Transcribe, scan or photocopy hard copy documents and drives.",
+
+                "Perform any other office tasks that management requires assistance with (checking/cleaning machine if needed, answering phones, etc.)",
+
+                "Report any major errors or inconsistencies to upper management and maintain report logs of in-progress and/or completed work.",
+
+                "Adhere to and meet set schedules and deadlines by input, track, and maintain all encoded data.",
+
+                "Self-audit my work checking for errors or duplication.",
+              ]}
             />
           </div>
           <div>
-            <ExperienceCard 
-              position = "Restaurant Worker / Service Crew"
-              year = "(Oct 2017 - Dec 2018)"
-              company = "McDonald’s Emirates, Emirates Fast Food Company L.L.C.– Sharjah, U. A. E."
-              description = {
-                <ul className="list-disc">
-                  <li>
-                    <p>
-                      Respond to questions, concerns, and complaints from customers, vendors, or clients.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Arrive to my shift on time and prioritize important tasks then manage my time effectively.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Escalate any problems or complaints to the relevant supervisor or manager.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Operate equipment relative to my role.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Maintain professionalism and a positive attitude.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Work and communicate effectively with co-workers and management.
-                    </p>
-                  </li>
-                </ul>
-              }
+            <ExperienceCard
+              position="Restaurant Worker / Service Crew"
+              year="(Oct 2017 - Dec 2018)"
+              company="McDonald’s Emirates, Emirates Fast Food Company L.L.C.– Sharjah, U. A. E."
+              description={[
+                "Respond to questions, concerns, and complaints from customers, vendors, or clients.",
+
+                "Arrive to my shift on time and prioritize important tasks then manage my time effectively.",
+
+                "Escalate any problems or complaints to the relevant supervisor or manager.",
+
+                "Operate equipment relative to my role.",
+
+                "Maintain professionalism and a positive attitude.",
+
+                "Work and communicate effectively with co-workers and management.",
+              ]}
             />
           </div>
           <div>
-            <ExperienceCard 
-              position = "Database Management System Cum Computer Networking – Internship"
-              year = " (Nov 2016 - Mar 2017)"
-              company = "Regional Trial Court (Branch 37) – Nueva Ecija, Philippines"
-              description = {
-                <ul className="list-disc">
-                  <li>
-                    <p>
-                    Identify areas where the application can be optimized for better performance, such as reducing memory usage, improving response times, or optimizing algorithms.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Implement appropriate security measures to protect the application and user data, such as encryption, authentication, and access control, depending on the application's requirements.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Create a detailed design plan for the desktop application, including its architecture, user interface, data structures, and algorithms. This involves making decisions on programming languages, frameworks, and tools to be used.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Conduct thorough testing of the application to identify and fix any bugs or issues. This includes unit testing, integration testing, and performing debugging tasks to ensure the application functions correctly.
-                    </p>
-                  </li>
-                </ul>
-              }
+            <ExperienceCard
+              position="Database Management System Cum Computer Networking – Internship"
+              year="(Nov 2016 - Mar 2017)"
+              company="Regional Trial Court (Branch 37) – Nueva Ecija, Philippines"
+              description={[
+                "Identify areas where the application can be optimized for better performance, such as reducing memory usage, improving response times, or optimizing algorithms.",
+
+                "Implement appropriate security measures to protect the application and user data, such as encryption, authentication, and access control, depending on the application's requirements.",
+
+                "Create a detailed design plan for the desktop application, including its architecture, user interface, data structures, and algorithms. This involves making decisions on programming languages, frameworks, and tools to be used.",
+
+                "Conduct thorough testing of the application to identify and fix any bugs or issues. This includes unit testing, integration testing, and performing debugging tasks to ensure the application functions correctly.",
+              ]}
             />
           </div>
         </Slider>
--- src/components/resume/ExperienceCard.js
+++ src/components/resume/ExperienceCard.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const ExperienceCard = ({ position, year, company, description }) => {
   return (
-    <div className="w-full h-[120vh] sm:h-[100vh] md:h-[100vh] xl:h-[90vh] flex items-center justify-center">
-      <div className="w-full h-auto md:w-[90%] mdl:w-[85%] flex justify-center items-center py-5 lg:mx-32 px-5">
-        <div className="w-full h-full flex items-center justify-center">
-          <div className="w-full h-full bg-black bg-opacity-20 hover:bg-opacity-30 duration-300 rounded-lg p-5 lg:px-10 flex justify-center items-center gap-5 shadow-shadowOne group">
-            <div className="top-0 w-full h-full justify-center items-center duration-300 opacity-100 p-5 ">
+    <div className="w-full flex items-center justify-center">
+      <div className="w-full md:w-[90%] mdl:w-[85%] flex justify-center items-center py-5 lg:mx-32 px-5">
+        <div className="w-full flex items-center justify-center">
+          <div className="w-full bg-black bg-opacity-20 hover:bg-opacity-30 duration-300 rounded-lg p-5 lg:px-10 flex justify-center items-center gap-5 shadow-shadowOne group">
+            <div className="w-full justify-center items-center duration-300 opacity-100 p-5 ">
               <div>
                 <h3 className="text-lg sm:text-xl lg:text-2xl font-semibold flex justify-left group-hover:text-designColor">
                   {position}
@@ -18,12 +18,18 @@ const ExperienceCard = ({ position, year, company, description }) => {
                 </p>
               </div>
               <div className="">
-                <p className="text-md lg:text-lg mt-2 text-gray-400 group-hover:text-white flex justify-left">
+                <p className="text-sm lg:text-lg mt-2 text-gray-400 group-hover:text-white flex justify-left">
                   {company}
                 </p>
               </div>
               <div className="text-sm md:text-base text-justify font-medium textgray-400 group-hover:text-gray-300 mt-10">
-                {description}
+                <ul className="list-disc">
+                  {description.map((items, index) => (
+                    <li key={index}>
+                      <p>{items}</p>
+                    </li>
+                  ) )}
+                </ul>
               </div>
               <span>
                 <div className="w-full border-b-[2px] border-b-designColor mt-10"></div>
